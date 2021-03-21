b130f2dbd613f47fd54d64c71b6e6558e4617e93
Tochukwu Ezechi <tezechi@toadeznet.com>
2021-03-21 14:03:01 +0100

    Make better for Get Product5

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 //====LIST DEPENDENCIES===//
-require("dotenv").config();
+// require("dotenv").config();
 const express = require('express');
 const parseurl = require('parseurl');
 const bodyParser = require('body-parser');
